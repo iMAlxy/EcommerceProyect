b61821e7a6f2b3173bb159240cf2e404dd0eff6a
Alexey Martínez <alexey.am@hotmail.com>
2023-07-28 21:05:09 -0600

    Editamos el css para agrearle el aside de carrito de compras. Obiamente ese fue optimizado para tratar de hacer una integración más limpia. Estamos en proceso

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuOnEmail = document.querySelector('.nav__email>img');
 const arrowMenu = document.querySelector('.change')
 const menuByDesktop = document.querySelector('.menu-desktop');
+const BurguerIcon = document.querySelector('.nav__menu');
+const menuByMobile = document.querySelector('.menu-mobile');
 
 menuOnEmail.addEventListener('click', toggleMenuDesktop);
+BurguerIcon.addEventListener('click', toggleMenuMobile);
 
 function toggleMenuDesktop() {
-    menuByDesktop.classList.toggle('inactive');
+    menuByDesktop.classList.toggle('slide-down');
+}
+
+function toggleMenuMobile() {
+    menuByMobile.classList.toggle('slide-right');
 }
