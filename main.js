9eb737d3ef3c0c8114ac01f8740804fed6b13e69
iMAlxey <alexey.am@hotmail.com>
2023-08-17 19:49:56 -0600

    Finalizamos de programar el comportamiento que tendrán los menús al desplegarse y cómo éstos ocultarán a los otros.

--- main.js
+++ main.js
@@ -3,14 +3,31 @@ const arrowMenu = document.querySelector('.change')
 const menuByDesktop = document.querySelector('.menu-desktop');
 const BurguerIcon = document.querySelector('.nav__menu');
 const menuByMobile = document.querySelector('.menu-mobile');
+const iconCart = document.querySelector('.nav__shopping-cart')
+const asideShopping = document.querySelector('.shopping-cart__container')
+const closeAsideIcon = document.querySelector('.mini-nav img')
 
 menuOnEmail.addEventListener('click', toggleMenuDesktop);
 BurguerIcon.addEventListener('click', toggleMenuMobile);
+iconCart.addEventListener('click', toggleShoppingAside);
+closeAsideIcon.addEventListener('click', closeShoppingAside);
 
 function toggleMenuDesktop() {
     menuByDesktop.classList.toggle('slide-down');
+    menuByMobile.classList.remove('slide-right');
 }
 
 function toggleMenuMobile() {
     menuByMobile.classList.toggle('slide-right');
+    menuByDesktop.classList.remove('slide-down');
+    asideShopping.classList.remove('slide-down');
+}
+
+function toggleShoppingAside() {
+    asideShopping.classList.toggle('slide-down')
+    menuByMobile.classList.remove('slide-right');
+}
+
+function closeShoppingAside() { 
+    asideShopping.classList.remove('slide-down');
 }
